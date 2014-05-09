50d7c548ac146788b90ab636d23a5ba4128e50a4
Pavlo Vdovych <pavlo.vdovych@gmail.com>
2014-05-09 14:03:06 +0300

    Experimenting with Airbnb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "pages#home" #get 'pages/home'
   get "about" => "pages#about"
   get "search_page" => "pages#search_page" 
+  get "experiment" => "pages#experiment" 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
