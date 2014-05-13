1f8e5d1d143cc6a1b739923f13006fad81e2f09e
Pavlo Vdovych <pavlo.vdovych@gmail.com>
2014-05-13 23:40:22 +0300

    Postponed Amazon S3 services to host my pictures for heroku

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'devise'
 
 gem 'paperclip', '~> 3.0'
 
+gem 'aws-sdk', '~> 1.20.0'
+
 group :development, :test do 
 # Use sqlite3 as the database for Active Record
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,17 @@ Rails.application.configure do
 
   # config/environments/production.rb
 
+  #Sets paperclip to upload images to Amazon S3
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
+
 
 end
