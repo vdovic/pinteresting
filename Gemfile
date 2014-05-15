f606b23ce0be8dacec2bb82ca6f7f61cdb64c751
Pavlo Vdovych <pavlo.vdovych@gmail.com>
2014-05-15 15:36:19 +0300

    Added pagination to pin_index

--- Gemfile
+++ Gemfile
@@ -31,6 +31,10 @@ gem 'devise'
 
 gem 'paperclip', '~> 3.0'
 
+gem 'will_paginate', '~> 3.0.5'
+
+gem 'will_paginate-bootstrap'
+
 group :development, :test do 
 # Use sqlite3 as the database for Active Record
 	gem 'sqlite3'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,9 +3,9 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
 
-  def index
-    @pins = Pin.all.order "created_at DESC"
-  end
+def index
+   @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+ end
 
   def show
   end
