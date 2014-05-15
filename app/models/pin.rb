a13d5cc3412f9e05a75046d1a7712499d635dc95
Pavlo Vdovych <pavlo.vdovych@gmail.com>
2014-05-15 11:13:28 +0300

    Add names to users instead of emails

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,8 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://media.bvb.edu/events/sites/default/files/default_images/defaultImg.jpg"
+
+  		validates :image, presence: true
+  		validates :description, presence: true
+
 end
